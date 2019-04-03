02ecfca8b115cbd882ef9ac2265ee4cb380b379c
EskalonaD <eskalonad@gmail.com>
2019-04-03 15:34:01 +0300

    add js-file/create quotes pool

---
+++ script.js
@@ -0,0 +1,44 @@
+const quotes = [
+	{
+		quote: "The only thing to fear is fear itself.",
+		source: "Franklin Delano Roosevelt",
+		citation: "First Inaugural Address",
+		year: 1932,
+		tags: "Business"
+	},
+	{
+		quote: "That's one small step for man, one giant leap for mankind.",
+		source: "Neil Armstrong",
+		citation: "The moon",
+		year: 1969,
+		tags: "Space Travel",
+	},
+	{
+		quote: "It always seems impossible until it is done.",
+		source: "Nelson Mandela",
+		citation: "",
+		year: "1918 - 2013",
+		tags: "Politics",
+	},
+	{
+		quote: "Not everything that can be counted counts, and not everything that counts can be counted.",
+		source: "Albert Einstein",
+		citation: "",
+		year: "1879 - 1955",
+		tags: "Engineering",
+	},
+	{
+		quote: "What we think, we become",
+		source: "Buddha",
+		citation: "",
+		year: "",
+		tags: "Faith",
+	},
+	{
+		quote: "Dream big and dare to fail",
+		source: "Norman Vaughan",
+		citation: "",
+		year: "1905 - 2005",
+		tags: "Business",
+	},
+];
